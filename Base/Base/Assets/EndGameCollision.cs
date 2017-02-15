c3ae8f53f3db9756e2dcc4692d61803271982c17
Johannes Rasinkangas <jrasinkangas@mica.edu>
2017-02-15 01:00:20 -0500

    Greybox & Mechanics
    Updated greybox of level, added in the last of the mechanics. Ask Sam
    about the scene switch on collision

---
+++ Base/Base/Assets/EndGameCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameCollision : MonoBehaviour {
+
+    public string inputName;
+    public string NextScene;
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if(hit.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(NextScene);
+        }
+    }
+
+}
